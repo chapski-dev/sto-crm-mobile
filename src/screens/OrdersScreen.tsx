52ac6dc6ab73f781f60bd486ef75797b6e533cab
Sasha-khomko <homkoaleksandr180@gimail.com>
2024-12-16 20:19:14 +0300

    feat/ui-OrderCard, OrderScreen

--- src/screens/OrdersScreen.tsx
+++ src/screens/OrdersScreen.tsx
@@ -7,15 +7,34 @@ import {Title} from '../ui/Text';
 interface Order {
   id: string;
   title: string;
+  status: string;
+  date: string;
+  carBrand: string;
 }
 
 const DATA: Order[] = [
-  {id: '1', title: 'Первая карточка'},
-  {id: '2', title: 'Вторая карточка'},
-  {id: '3', title: 'Третья карточка'},
+  {
+    id: '1',
+    title: '№231128162535',
+    status: 'В работе',
+    date: '24 дек. 2023',
+    carBrand: 'Honda Jazz III Рестайлинг, Хэтчбек 5 дв. 8682 AX-3',
+  },
+  {
+    id: '2',
+    title: '№231128162535',
+    status: 'Отказ клиента',
+    date: '24 дек. 2023',
+    carBrand: 'Honda Jazz III Рестайлинг, Хэтчбек 5 дв. 8682 AX-3',
+  },
+  {
+    id: '3',
+    title: '№231128162535',
+    status: 'Выполнен',
+    date: '24 дек. 2023',
+    carBrand: 'Honda Jazz III Рестайлинг, Хэтчбек 5 дв. 8682 AX-3',
+  },
 ];
-
-// const DATA: Order[] = [];
 
 export const OrdersScreen: React.FC = () => {
   return (
@@ -23,22 +42,28 @@ export const OrdersScreen: React.FC = () => {
       <View style={styles.wrapperTitle}>
         <Title title="Список заказов" />
       </View>
-      {DATA.length > 0 ? (
-        <FlatList
-          data={DATA}
-          renderItem={({item}) => <OrderCard title={item.title} />}
-          keyExtractor={item => item.id}
-          style={styles.listContainer}
-        />
-      ) : (
-        <View style={styles.emptyStateContainer}>
-          <ReportSearchIcon />
-          <Text style={styles.text}>Пока пусто</Text>
-          <Text style={styles.prompting}>
-            Здесь будет отображаться список заказов
-          </Text>
-        </View>
-      )}
+      <FlatList
+        data={DATA}
+        renderItem={({item}) => (
+          <OrderCard
+            title={item.title}
+            status={item.status}
+            date={item.date}
+            carBrand={item.carBrand}
+          />
+        )}
+        keyExtractor={item => item.id}
+        ListEmptyComponent={
+          <View style={styles.emptyStateContainer}>
+            <ReportSearchIcon />
+            <Text style={styles.text}>Пока пусто</Text>
+            <Text style={styles.prompting}>
+              Здесь будет отображаться список заказов
+            </Text>
+          </View>
+        }
+        contentContainerStyle={styles.contentContainer}
+      />
     </View>
   );
 };
@@ -52,16 +77,14 @@ const styles = StyleSheet.create({
     marginTop: 8,
     marginLeft: 16,
   },
-  listContainer: {
-    flex: 1,
-    left: 16,
-    gap: 50,
-  },
   emptyStateContainer: {
-    width: '100%',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  contentContainer: {
+    flexGrow: 1,
+    justifyContent: DATA.length === 0 ? 'center' : 'flex-start',
   },
   text: {
     fontWeight: '600',
@@ -72,7 +95,6 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: '400',
     lineHeight: 20,
-    alignItems: 'center',
     textAlign: 'center',
     width: '50%',
   },
--- src/ui/OrderCard.tsx
+++ src/ui/OrderCard.tsx
@@ -1,17 +1,94 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-export const OrderCard = ({title}) => {
+interface OrderCardProps {
+  title: string;
+  status: string;
+  date: string;
+  carBrand: string;
+}
+
+export const OrderCard: React.FC<OrderCardProps> = ({
+  title,
+  status,
+  date,
+  carBrand,
+}) => {
+  const getStatusStyle = () => {
+    switch (status) {
+      case 'В работе':
+        return styles.statusInProgress;
+      case 'Отказ клиента':
+        return styles.statusClientRefused;
+      case 'Выполнен':
+        return styles.statusCompleted;
+      default:
+        return {};
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>{title}</Text>
+      <View style={styles.wrapper}>
+        <View>
+          <Text style={styles.title}>{title}</Text>
+          <Text>{date}</Text>
+        </View>
+        <View style={[styles.wrapperStatus, getStatusStyle()]}>
+          <Text style={[styles.statusText, getStatusStyle()]}>{status}</Text>
+        </View>
+      </View>
+      <View>
+        <Text style={styles.carBrand}>{carBrand}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    paddingTop: 20,
+    paddingTop: 16,
+    paddingBottom: 16,
+    gap: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: '#E8ECF0',
+    marginHorizontal: 16,
+  },
+  wrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 15,
+    lineHeight: 22,
+  },
+  wrapperStatus: {
+    paddingLeft: 8,
+    paddingTop: 4,
+    paddingRight: 8,
+    paddingBottom: 4,
+    borderRadius: 8,
+    alignSelf: 'flex-start',
+  },
+  statusText: {
+    fontSize: 15,
+    lineHeight: 22,
+  },
+  statusInProgress: {
+    backgroundColor: '#E0F2FF',
+    color: '#0084EB',
+  },
+  statusClientRefused: {
+    backgroundColor: '#FFE9E5',
+    color: '#EB4221',
+  },
+  statusCompleted: {
+    backgroundColor: '#E9F7E5',
+    color: '#42B121',
+  },
+  carBrand: {
+    fontSize: 15,
+    lineHeight: 22,
+    color: '#0084EB',
   },
 });
