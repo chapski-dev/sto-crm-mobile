3259372cbcfe4008e07815512a363af29537cc9e
highpusy <pticabyrde@gmail.com>
2024-12-17 15:42:20 +0300

    cleaned up the app component

--- App.tsx
+++ App.tsx
@@ -1,28 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigator} from './src/navigation/Navigator';
-import { SafeAreaView } from "react-native";
-import { Auth } from "./src/screens/Auth";
-import { useState } from "react";
-import { NoConnection } from "./src/screens/NoConnection";
 import React from "react";
 
 function App(): React.JSX.Element {
 
-  const [screenState, setScreenState] = useState('no connection')
-
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        {
-          screenState === 'no connection' 
-          ?
-          <NoConnection />
-          :
-          <Auth />
-        }
-      </SafeAreaView>
 
       <Navigator />
+
     </NavigationContainer>
 
   );
